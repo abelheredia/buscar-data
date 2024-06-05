341c937d79e7badaea173b658aa02dca37dcf871
xt8263 <xt8263@rimac.com.pe>
2024-06-05 16:12:55 -0500

    v1.0.0

--- src/App.tsx
+++ src/App.tsx
@@ -5,10 +5,12 @@ function App() {
   
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<any>(null);
+  const [tipo_doc, setTipoDoc] = useState('dni');
  
   const search = () => {
     setLoading(true);
     const tipo_doc = (document.querySelector('select[name="tipo_doc"]') as HTMLSelectElement).value;
+    setTipoDoc(tipo_doc);
     const documento = (document.querySelector('input[name="documento"]') as HTMLInputElement).value;
 
     const requestOptions = {
@@ -38,50 +40,66 @@ function App() {
       {
         loading ? 
         <>
-        <img src="loading.gif" alt="logo" />
-        cargando...
+          cargando...
         </>
         : !data ?
         <>
-        <h4>Buscar Data</h4>
-        <div className="doc">
-          <select name='tipo_doc'>
-            <option value="dni">DNI</option>
-            <option value="ruc">RUC</option>
-          </select>
-          <input type="number" name='documento' />
-        </div>
-        <button onClick={search} >Buscar</button>
+          <h4>Buscar Data</h4>
+          <div className="doc">
+            <select name='tipo_doc'>
+              <option value="dni">DNI</option>
+              <option value="ruc">RUC</option>
+            </select>
+            <input type="number" name='documento' />
+          </div>
+          <button onClick={search} >Buscar</button>
         </>
         :
         <>
-        <div className="data">
-          <div>
-            <span>Documento: </span>
-            <span>{ data.numero }</span>
+          <div className="data">
+            {
+              tipo_doc === 'dni' ?
+              <>
+                <div>
+                  <span>Documento: </span>
+                  <span>{ data.numero }</span>
+                </div>
+                <div>
+                  <span>Nombres: </span>
+                  <span>{ data.nombres }</span>
+                </div>
+                <div>
+                  <span>Apellido Paterno: </span>
+                  <span>{ data.apellido_paterno }</span>
+                </div>
+                <div>
+                  <span>Apellido Materno: </span>
+                  <span>{ data.apellido_materno }</span>
+                </div>
+                <div>
+                  <span>Fecha de Nacimiento: </span>
+                  <span>{ data.fecha_nacimiento }</span>
+                </div>
+                <div>
+                  <span>Sexo: </span>
+                  <span>{ data.sexo }</span>
+                </div>
+              </>
+              :
+              <>
+                <div>
+                  <span>Documento: </span>
+                  <span>{ data.numero }</span>
+                </div>
+                <div>
+                  <span>Razón social: </span>
+                  <span>{ data.razonSocial }</span>
+                </div>
+
+              </>
+            }
           </div>
-          <div>
-            <span>Nombres: </span>
-            <span>{ data.nombres }</span>
-          </div>
-          <div>
-            <span>Apellido Paterno: </span>
-            <span>{ data.apellido_paterno }</span>
-          </div>
-          <div>
-            <span>Apellido Materno: </span>
-            <span>{ data.apellido_materno }</span>
-          </div>
-          <div>
-            <span>Fecha de Nacimiento: </span>
-            <span>{ data.fecha_nacimiento }</span>
-          </div>
-          <div>
-            <span>Sexo: </span>
-            <span>{ data.sexo }</span>
-          </div>
-        </div>
-        {/* <button onClick={clean} >Buscar</button> */}
+          <button onClick={clean} > Limpiar </button>
         </> 
       }
       </div>
